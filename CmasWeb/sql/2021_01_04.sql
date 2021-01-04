7c6a8625f91cf523d61c0814832aec6f55e031d1
Alexander Petukhov <apetukhov@accesssoftek.com>
2021-01-04 19:25:36 +0000

    new countries

---
+++ CmasWeb/sql/2021_01_04.sql
@@ -0,0 +1,32 @@
+INSERT INTO `countries`
+(`id`,
+ `deleted`,
+ `name`,
+ `version`,
+ `code`,
+ `iso3166_1_alpha_2_code`)
+VALUES (131,
+        false,
+        'Republic of Djibouti',
+        2,
+        "DJI",
+        "DJ");
+
+INSERT INTO `countries`
+(`id`,
+ `deleted`,
+ `name`,
+ `version`,
+ `code`,
+ `iso3166_1_alpha_2_code`)
+VALUES (132,
+        false,
+        'the Commonwealth of the Northern Mariana Islands',
+        3,
+        "MNP",
+        "MP");
+
+
+
+
+
